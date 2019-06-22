72f430c8cd8b1e5a659e3370eeb6de5e35c810d7
Dakota Chambers <dakota@imagemovermd.com>
2019-06-21 20:03:15 -0500

    Create/Open CHANGELOG file for writing

--- ezcl.rb
+++ ezcl.rb
@@ -1,9 +1,13 @@
 require 'git'
 
 path_to_git_repo = '.'
+changelog_file = File.open("CHANGELOG.md", "w")
 
 puts 'ezcl - Generating a changelog from your git log.'
+changelog_file.puts '# CHANGELOG'
+changelog_file.puts #newline
 
 g = Git.open(path_to_git_repo)
+g.log.each {|l| changelog_file.puts l.message }
 
-puts g.log
+changelog_file.close
