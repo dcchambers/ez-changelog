40318e67a11bfc542ebe755b042fdce7e888ae18
Dakota Chambers <dakota@imagemovermd.com>
2019-06-21 20:08:51 -0500

    Add feature to let user name changelog file

--- ezcl.rb
+++ ezcl.rb
@@ -1,9 +1,11 @@
 require 'git'
 
 path_to_git_repo = '.'
-changelog_file = File.open("CHANGELOG.md", "w")
 
 puts 'ezcl - Generating a changelog from your git log.'
+print 'Enter CHANGELOG file name: '
+filename = gets.chomp
+changelog_file = File.open(filename, "w")
 changelog_file.puts '# CHANGELOG'
 changelog_file.puts #newline
 
