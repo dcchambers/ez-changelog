2e3fe7ea67d933ed9512203203f3effe0b572ffc
Dakota Chambers <dakota@imagemovermd.com>
2019-06-21 20:28:41 -0500

    feat: Add time/date to changelog.

--- ezcl.rb
+++ ezcl.rb
@@ -10,6 +10,8 @@ path_to_git_repo = gets.chomp
 changelog_file = File.open(filename, "w")
 changelog_file.puts '# CHANGELOG'
 changelog_file.puts #newline
+changelog_file.print '## '
+changelog_file.puts Time.new.strftime("%Y-%m-%d %H:%M:%S")
 
 g = Git.open(path_to_git_repo)
 g.log.each {|l| changelog_file.puts "- "+l.message }
